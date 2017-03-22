31821827a239f2a8e42f528322237ab352e9a08d
Travis A <travis.m.albers@student.orecity.k12.or.us>
2017-03-22 08:56:04 -0700

    adds numbers together

--- Main.cpp
+++ Main.cpp
@@ -2,9 +2,34 @@
 
 using namespace std;
 
+int sum(int);
+
 int main()
 {
-	cout << "hello World" << endl;
+	int num = 0;
+	cout << "Enter a positive integer:  ";
+	cin >> num;
+	
+	cout << "The sum from 0 to " << num << " is " << sum(num) << endl;
 
 	return 0;
 }
+
+
+int sum(int number)
+{
+	if(number == 0)
+	{
+		return 0;
+	}
+	else if(number < 0)
+	{
+		cout << "You idiot, Thats a negative number" <<endl;
+		return -1;
+	}
+	else
+	{
+		return number+sum(number-1);
+	}
+}
+
